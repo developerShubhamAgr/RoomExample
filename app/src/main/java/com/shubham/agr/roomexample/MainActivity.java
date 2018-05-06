11960ac99e996702cfcb2a64d5aabc0937eb4158
Shubham Agrawal <shubham.agrawal20310@gmail.com>
2018-05-06 13:27:26 +0545

    Add MainActivity
    MainActivity is added with a empty recycler view and a floating action button.

--- app/src/main/java/com/shubham/agr/roomexample/MainActivity.java
+++ app/src/main/java/com/shubham/agr/roomexample/MainActivity.java
@@ -1,13 +1,25 @@
 package com.shubham.agr.roomexample;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(v->{
+            Log.d(TAG, "onCreate: FAB Pressed");
+        });
     }
 }
